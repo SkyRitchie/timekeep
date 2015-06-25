cc78bdd4ee0d114fd75f8d18268522709048cb11
schuyler Ritchie <schuylerelizabethritchie@gmail.com>
2015-06-25 06:19:54 -0400

    move array to seeds file and reference Watch.all

--- app/controllers/watches_controller.rb
+++ app/controllers/watches_controller.rb
@@ -1,11 +1,7 @@
 class WatchesController < ApplicationController
   def index
   	# pass an instance variable to the view
-  	@watches = [
-  		{name: "Backup Black", brand: "Swatch"},
-  		{name: "Classic Digital Casio", brand: "Casio"},
-  		{name: "1960s Vintage Raketa", brand: "Raketa"},
-  		{name: "Max Bill Chronoscope", brand: "Junghans"}]
+  	@watches = Watch.all
   	# rails is rendering the index view
   end
 
